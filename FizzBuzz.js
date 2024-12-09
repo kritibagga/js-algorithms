37a8dcb25fb33a4b92f9d22feed94d7d5687b781
kritibagga <bagga.kriti@gmail.com>
2024-12-09 15:59:26 -0500

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,11 @@
+//Write a function to print numbers from 1 to 100. For multiples of 3, print "Fizz"; for multiples of 5, print "Buzz"; for multiples of both, print "FizzBuzz".
+
+function fizzBuzzSimplified(){
+    for(let i=1;i<=100;i++){
+        if(i%3==0 && i%5==0) console.log("FizzBuzz");
+        else if(i%3==0) console.log("Fizz");
+        else if(i%5==0) console.log("Buzz");
+        else console.log(i);
+    }
+}
+fizzBuzzSimplified();
